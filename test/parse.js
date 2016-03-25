febbf977ae09bf1f395e96cb7a786deb57c29532
coderaiser <mnemonic.enemy@gmail.com>
2016-03-25 14:43:34 -0400

    test(parse) certPath

--- test/parse.js
+++ test/parse.js
@@ -2,6 +2,7 @@
 
 let docker      = require('..');
 let test        = require('tape');
+let tryCatch    = require('try-catch');
 
 test('parse: no args', (t) => {
     t.notOk(docker.parse(), 'should return nothing');
@@ -16,6 +17,22 @@ test('parse: socketPath', (t) => {
     let result = docker.parse(config);
     
     t.deepEqual(config, result, 'should return object with socketPath');
+    t.end();
+});
+
+test('parse: certPath', (t) => {
+    let fn = () => docker.parse(config);
+    let config = {
+        certPath: 'hello',
+        host: '192.168.99.100:2376'
+    };
+    
+    let error = tryCatch(() => {
+        fn();
+    });
+    t.ok(error, 'should be read error');
+    t.equal(error.code, 'ENOENT', 'no entry error');
+    
     t.end();
 });
 
