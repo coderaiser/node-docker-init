2577b7a0f24454d3d2fea735d6ff67dc608676a1
coderaiser <coderaiser@cloudcmd.io>
2017-06-16 14:40:37 +0300

    chore(read-file-sync) let -> const

--- test/read-file-sync.js
+++ test/read-file-sync.js
@@ -1,12 +1,12 @@
 'use strict';
 
-let path        = require('path');
-let fs          = require('fs');
+const path = require('path');
+const fs = require('fs');
 
-let docker      = require('..');
-let test        = require('tape');
-let sinon       = require('sinon');
-let tryCatch    = require('try-catch');
+const docker = require('..');
+const test = require('tape');
+const sinon = require('sinon');
+const tryCatch = require('try-catch');
 
 test('readFileSync: no arguments', (t) => {
     t.throws(docker.readFileSync, /certPath should be string!/, 'should throw when no certPath');
@@ -14,32 +14,32 @@ test('readFileSync: no arguments', (t) => {
 });
 
 test('readFileSync: no name', (t) => {
-    let fn = () => docker.readFileSync('hello');
+    const fn = () => docker.readFileSync('hello');
     
     t.throws(fn, /name should be string!/, 'should throw when no name');
     t.end();
 });
 
 test('readFileSync: should throw when no file', (t) => {
-    let fn = () => docker.readFileSync('hello', 'world');
+    const fn = () => docker.readFileSync('hello', 'world');
     
     t.throws(fn, /Error: ENOENT: no such file or directory, open 'hello\/world\.pem'/, 'should throw when no such file');
     t.end();
 });
 
 test('readFileSync: should convert path with "~" in certPath', (t) => {
-    let name = String(Math.random());
-    let fn = () => docker.readFileSync('~/hello', name);
+    const name = String(Math.random());
+    const fn = () => docker.readFileSync('~/hello', name);
     
-    let error = tryCatch(fn);
+    const error = tryCatch(fn);
     t.ok(error, 'should be error');
     t.notOk(error.message.includes('~'), 'should not contain "~"');
     t.end();
 });
 
 test('readFileSync: path.join', (t) => {
-    let fn = () => docker.readFileSync('hello', 'world');
-    let spy = sinon.spy(path, 'join');
+    const fn = () => docker.readFileSync('hello', 'world');
+    const spy = sinon.spy(path, 'join');
     
     tryCatch(fn);
     
@@ -48,8 +48,8 @@ test('readFileSync: path.join', (t) => {
 });
 
 test('readFileSync: fs.readFileSync', (t) => {
-    let fn = () => docker.readFileSync('hello', 'world');
-    let spy = sinon.spy(fs, 'readFileSync');
+    const fn = () => docker.readFileSync('hello', 'world');
+    const spy = sinon.spy(fs, 'readFileSync');
     
     tryCatch(fn);
     
